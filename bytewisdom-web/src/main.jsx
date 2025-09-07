4ce339193036bf6d68a9742224df717e45bd483f
nramirez4545 <nramirez4545@gmail.com>
2025-09-06 19:50:09 -0500

    Fix import path for App.jsx

--- bytewisdom-web/src/main.jsx
+++ bytewisdom-web/src/main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './pages/App.jsx'
-import About from './pages/About.jsx'
+
+import App from './App.jsx'           // ← was ./pages/App.jsx
+import About from './pages/About.jsx' // this one is fine
+
 import './index.css'
 
 const router = createBrowserRouter([
@@ -11,5 +13,7 @@ const router = createBrowserRouter([
 ])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><RouterProvider router={router} /></React.StrictMode>
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
 )
